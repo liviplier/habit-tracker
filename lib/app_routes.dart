31fe7b87a7702a269fa505d758819351c22ad168
liviplier <76095476+liviplier@users.noreply.github.com>
2022-02-24 21:33:07 +0300

    Delete lib directory

--- lib/app_routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const welcome = "/welcome";
-  static const login = "/login";
-  static const signup = "/signup";
-  static const forgotpassword = "/forgotpassword";
-  static const home = "/home";
-  static const main = "/main";
-}
